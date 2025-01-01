46eaf91d16c9b447a1b0312c018c27c2be089808
Thiago Santos <programadorthi@gmail.com>
2025-01-01 06:53:55 -0300

    Basic native project

---
+++ app/src/main/native/main.c
@@ -0,0 +1,10 @@
+#include <android/log.h>
+#include <android_native_app_glue.h>
+
+#define LOG_TAG "NativeApplication"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+void android_main(struct android_app* app)
+{
+    LOGI( ">>>> Starting...\n" );
+}
