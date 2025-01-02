b3c95f83e6b9d4c9e735b20ddd083f035aacc366
Thiago Santos <programadorthi@gmail.com>
2025-01-02 11:27:27 -0300

    More basic setup

--- app/src/main/native/main.c
+++ app/src/main/native/main.c
@@ -4,6 +4,7 @@
 #define LOG_TAG "NativeApplication"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
+__attribute__((unused))
 void android_main(struct android_app* app)
 {
     LOGI( ">>>> Starting...\n" );
