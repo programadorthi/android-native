e28a26562719adf135304731b0870fa8959f3687
Thiago Santos <programadorthi@gmail.com>
2025-01-06 19:15:09 -0300

    LVGL code improvements to work on Android 24+

--- app/src/main/native/main.c
+++ app/src/main/native/main.c
@@ -3,60 +3,44 @@
 
 #include "lvgl.h"
 
+/*********************
+ *      DEFINES
+ *********************/
 #define LOG_TAG "NativeApplication"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-#define BYTE_PER_PIXEL (LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_ARGB8888))
+// TODO: Dynamic buffer size based on W x H is not working on Andorid 25+
+// TODO: Implement dynamic buffer size based on device resolution to support res > 2048
+#define BUF_SIZE (2048 * 2048 * (LV_COLOR_DEPTH + 7) / 8)
 
-static ANativeWindow *nativeWindow;
-static pthread_t lvglTickThread;
-static bool ready = false;
+/**********************
+ *      TYPEDEFS
+ **********************/
 
-static void handleCmd(struct android_app *app, int32_t cmd);
+struct app_data_t {
+    ANativeWindow *window;  // Android window object
+    pthread_t tickThread; // Tick thread
+    bool running; // Flag to stop tick thread
+};
 
-static void initLvgl(struct android_app *app);
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+// Double buffering
+static uint8_t buf1[BUF_SIZE];
+static uint8_t buf2[BUF_SIZE];
 
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+// TODO: maybe an extern to decouple from main.c file
 static void render();
-
-__attribute__((unused))
-void android_main(struct android_app *app) {
-    lv_init();
-
-    app->onAppCmd = handleCmd;
-
-    int *events = NULL;
-    struct android_poll_source *source;
-    while (!app->destroyRequested) {
-        int timeout = ready ? 0 : -1;
-        int result = ALooper_pollOnce(timeout, NULL, events, (void **) &source);
-        if (result == ALOOPER_POLL_ERROR) {
-            LOGE("ALooper_pollOnce returned error %d\n", result);
-            break;
-        }
-        if (source) {
-            source->process(app, source);
-        }
-    }
-
-    lv_deinit();
-}
-
-static void handleCmd(struct android_app *app, int32_t cmd) {
-    switch (cmd) {
-        case APP_CMD_INIT_WINDOW:
-            ready = true;
-            initLvgl(app);
-            break;
-        case APP_CMD_TERM_WINDOW:
-            ready = false;
-            break;
-        default:
-            // TODO: screen rotation font size not good
-            break;
-
-    }
-}
 
 static uint32_t currentTimeInMillis(void) {
     struct timespec t;
@@ -65,18 +49,22 @@ static uint32_t currentTimeInMillis(void) {
     return timeMs;
 }
 
-static void *timerHandler(__attribute__((unused)) void *data) {
-    while (true) {
-        if (!ready) {
-            break;
-        }
+static void *refresh_routine(void *data) {
+    struct app_data_t *app_data = data;
+    LV_ASSERT_NULL(app_data);
+
+    while (app_data->running) {
         uint32_t timeUntilNext = lv_timer_handler();
         lv_delay_ms(timeUntilNext);
     }
-    return 0;
+
+    return NULL;
 }
 
-static void pixelFlush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
+static void flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
+    struct app_data_t *data = lv_display_get_driver_data(disp);
+    LV_ASSERT_NULL(data);
+
     uint32_t areaWidth = lv_area_get_width(area);
     uint32_t areaHeight = lv_area_get_height(area);
 
@@ -86,19 +74,20 @@ static void pixelFlush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_ma
     int32_t bottom = LV_MIN(area->y2 + 1, areaHeight);
 
     ANativeWindow_Buffer buffer;
-    ANativeWindow_lock(nativeWindow, &buffer, 0);
+    ANativeWindow_lock(data->window, &buffer, 0);
 
     int32_t width = LV_MIN(buffer.width, (right - left));
     int32_t height = LV_MIN(buffer.height, (bottom - top));
 
     uint32_t *out = (uint32_t *) buffer.bits;
 
+    // TODO: There is a faster way to copy pixels? I don't know yet.
     for (int32_t y = 0; y < height; y++) {
         for (int32_t x = 0; x < width; x++) {
             uint8_t b = *(px_map++); // px_map[0] has blue color
-            uint8_t g = *(px_map++); // px_map[0] has green color
-            uint8_t r = *(px_map++); // px_map[0] has red color
-            uint8_t a = *(px_map++); // px_map[0] has alpha opacity
+            uint8_t g = *(px_map++); // px_map[1] has green color
+            uint8_t r = *(px_map++); // px_map[2] has red color
+            uint8_t a = *(px_map++); // px_map[3] has alpha opacity
 
             uint32_t pixel = (a << 24) | (b << 16) | (g << 8) | r;
             out[x] = pixel;
@@ -106,38 +95,25 @@ static void pixelFlush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_ma
         out += buffer.stride;
     }
 
-    ANativeWindow_unlockAndPost(nativeWindow);
+    ANativeWindow_unlockAndPost(data->window);
     lv_display_flush_ready(disp);
 }
 
-static void initLvgl(struct android_app *app) {
-    nativeWindow = app->window;
+static void close_display(struct app_data_t *data) {
+    data->running = false;
+    void *ret = NULL;
+    pthread_join(data->tickThread, &ret);
+}
 
-    lv_tick_set_cb(currentTimeInMillis);
+static void open_display(struct app_data_t *data, int32_t dpi) {
+    int32_t hor_res = ANativeWindow_getWidth(data->window);
+    int32_t ver_res = ANativeWindow_getHeight(data->window);
 
-    AConfiguration *config = AConfiguration_new();
-    AConfiguration_fromAssetManager(config, app->activity->assetManager);
-    int32_t dpi = AConfiguration_getDensity(config);
-    AConfiguration_delete(config);
-
-    int32_t windowWidth = ANativeWindow_getWidth(nativeWindow);
-    int32_t windowHeight = ANativeWindow_getHeight(nativeWindow);
-
-    lv_display_t *display = lv_display_create(windowWidth, windowHeight);
+    lv_display_t *display = lv_display_create(hor_res, ver_res);
+    lv_display_set_driver_data(display, data);
     lv_display_set_dpi(display, dpi);
-    lv_display_set_flush_cb(display, pixelFlush);
-
-    uint32_t buf1[windowWidth * windowHeight * BYTE_PER_PIXEL];
-    uint32_t buf2[windowWidth * windowHeight * BYTE_PER_PIXEL];
-    lv_display_set_buffers(display, buf1, buf2, sizeof(buf1), LV_DISPLAY_RENDER_MODE_FULL);
-
-    ANativeWindow_setBuffersGeometry(nativeWindow, windowWidth, windowHeight,
-                                     WINDOW_FORMAT_RGBA_8888);
-
-    ANativeWindow_Buffer buffer;
-    ANativeWindow_lock(nativeWindow, &buffer, 0);
-    lv_memset(buffer.bits, 0xFF, buffer.stride * buffer.height * 4);
-    ANativeWindow_unlockAndPost(nativeWindow);
+    lv_display_set_flush_cb(display, flush_cb);
+    lv_display_set_buffers(display, buf1, buf2, BUF_SIZE, LV_DISPLAY_RENDER_MODE_FULL);
 
     lv_theme_t *theme = lv_theme_default_init(
             display,
@@ -150,20 +126,108 @@ static void initLvgl(struct android_app *app) {
 
     render();
 
-    pthread_create(&lvglTickThread, 0, timerHandler, 0);
+    data->running = true;
+    pthread_create(&data->tickThread, NULL, refresh_routine, data);
 }
 
 static void render() {
-    /*Change the active screen's background color*/
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
+    lv_obj_t *display = lv_screen_active();
 
-    /*Create a white label, set its text and align it to the center*/
-    lv_obj_t *label = lv_label_create(lv_screen_active());
+    struct app_data_t *data = lv_display_get_driver_data(lv_display_get_default());
+    LV_ASSERT_NULL(data);
 
-    // TODO: label circular animation is crashing after a while
+    //*Change the active screen's background color*//*
+    lv_obj_set_style_bg_color(display, lv_color_hex(0x003a57), LV_PART_MAIN);
+
+    //*Create a white label, set its text and align it to the center*//*
+    lv_obj_t *label = lv_label_create(display);
+
     lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
-    lv_obj_set_width(label, ANativeWindow_getWidth(nativeWindow));
-    lv_label_set_text(label, "Lorem Ipsum is simply dummy text of the printing and typesetting industry");
-    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_set_width(label, ANativeWindow_getWidth(data->window));
+    lv_label_set_text(label,
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry");
+    lv_obj_set_style_text_color(display, lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
+/*static void lvgl_log_cb(lv_log_level_t level, const char *buf) {
+    if (level == LV_LOG_LEVEL_ERROR) {
+        LOGE(">> LVGL: %s", buf);
+    } else {
+        LOGI(">> LVGL: %s", buf);
+    }
+}*/
+
+/**********************
+ *   ANDROID FUNCTIONS
+ **********************/
+static void handle_cmd(struct android_app *app, int32_t cmd) {
+    static int32_t format = WINDOW_FORMAT_RGB_565;
+    struct app_data_t *data = (struct app_data_t *) app->userData;
+
+    switch (cmd) {
+        case APP_CMD_INIT_WINDOW:
+            data->window = app->window;
+
+            AConfiguration *config = AConfiguration_new();
+            AConfiguration_fromAssetManager(config, app->activity->assetManager);
+            int32_t dpi = AConfiguration_getDensity(config);
+            AConfiguration_delete(config);
+
+            format = ANativeWindow_getFormat(data->window);
+            ANativeWindow_setBuffersGeometry(
+                    data->window,
+                    ANativeWindow_getWidth(data->window),
+                    ANativeWindow_getHeight(data->window),
+                    WINDOW_FORMAT_RGBA_8888
+            );
+
+            open_display(data, dpi);
+            break;
+        case APP_CMD_TERM_WINDOW:
+            // FIXME: ANDROID <=26 call APP_CMD_TERM_WINDOW during screen rotation :/
+            close_display(data);
+            ANativeWindow_setBuffersGeometry(
+                    data->window,
+                    ANativeWindow_getWidth(data->window),
+                    ANativeWindow_getHeight(data->window),
+                    format
+            );
+            break;
+        default:
+            // TODO: screen rotation font size not good
+            break;
+
+    }
+}
+
+__attribute__((unused))
+void android_main(struct android_app *app) {
+    struct app_data_t app_data;
+    lv_memzero(&app_data, sizeof(app_data));
+
+    lv_init();
+
+    //lv_log_register_print_cb(lvgl_log_cb);
+
+    lv_tick_set_cb(currentTimeInMillis);
+
+    app->userData = &app_data;
+    app->onAppCmd = handle_cmd;
+
+    int *events = NULL;
+    struct android_poll_source *source = NULL;
+    while (!app->destroyRequested) {
+        int timeout = app_data.running ? 0 : -1;
+        int result = ALooper_pollOnce(timeout, NULL, events, (void **) &source);
+        if (result == ALOOPER_POLL_ERROR) {
+            LOGE("ALooper_pollOnce returned error %d\n", result);
+            break;
+        }
+        if (source) {
+            source->process(app, source);
+        }
+    }
+
+    lv_deinit();
 }
