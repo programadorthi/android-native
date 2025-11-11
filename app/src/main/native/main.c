633a8eba9dec840a55c5adc5ea14bd395ccbc34a
Thiago Santos <programadorthi@gmail.com>
2025-11-11 17:51:52 -0300

    Initial raylib submodule setup

--- app/src/main/native/main.c
+++ app/src/main/native/main.c
@@ -1,5 +1,6 @@
 #include <android/log.h>
 #include <android_native_app_glue.h>
+#include "raylib.h"
 
 #define LOG_TAG "NativeApplication"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
